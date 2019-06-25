d53873e23e1f1bc6c91dee87ce7e623f60210af5
Opity <frola.filip@gmail.com>
2019-06-25 13:33:46 +0200

    oprava

--- xForce_rest_test.ps1
+++ xForce_rest_test.ps1
@@ -1,5 +1,5 @@
 
-$hdrs = @{"apikey" = "e042f30f-2917-4076-a10f-71cb3bcd61e5" ; "password"= "c5dec9c8-8f7d-47e4-90f6-9157e20df947"}
+$hdrs = @{"apikey" = "apikey" ; "password"= "password"}
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-RestMethod -Method Get -Uri 'https://api.xforce.ibmcloud.com/casefiles/public'  -Headers $hdrs >> .\content.txt
